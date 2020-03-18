58863017fb921c3cf25b9496717dd2bdf8890dde
irene macapinlac <irenekaymac@gmail.com>
2020-03-18 23:31:32 +1100

    2

---
+++ build/precache-manifest.0b3794f5dda3e7e2c28d3927c09260de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3890bee4897cac5750456315d8fe4892",
+    "url": "/index.html"
+  },
+  {
+    "revision": "210b5068f9b505ac5390",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "bd246cf9c280d2e30292",
+    "url": "/static/css/main.8a8c4e58.chunk.css"
+  },
+  {
+    "revision": "210b5068f9b505ac5390",
+    "url": "/static/js/2.c33df47c.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/static/js/2.c33df47c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd246cf9c280d2e30292",
+    "url": "/static/js/main.0c9762a9.chunk.js"
+  },
+  {
+    "revision": "8b9ea6f5353ff908896f",
+    "url": "/static/js/runtime-main.89a54064.js"
+  }
+]);
