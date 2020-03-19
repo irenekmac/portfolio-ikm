31f6c81c1f54abe40aa60f9febb340e369245d14
irene macapinlac <irenekaymac@gmail.com>
2020-03-19 22:37:49 +1100

    Updates

---
+++ precache-manifest.7ab8729c963be7fd1965fcebccc1d164.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abbed2bb8ce8763420b639ba755918c9",
+    "url": "/portfolio-ikm/index.html"
+  },
+  {
+    "revision": "210b5068f9b505ac5390",
+    "url": "/portfolio-ikm/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "f2c3453fdb72ecdcab57",
+    "url": "/portfolio-ikm/static/css/main.a064b293.chunk.css"
+  },
+  {
+    "revision": "210b5068f9b505ac5390",
+    "url": "/portfolio-ikm/static/js/2.c33df47c.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/portfolio-ikm/static/js/2.c33df47c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2c3453fdb72ecdcab57",
+    "url": "/portfolio-ikm/static/js/main.3262fc84.chunk.js"
+  },
+  {
+    "revision": "a090c8ac196a651727a9",
+    "url": "/portfolio-ikm/static/js/runtime-main.b5823f6b.js"
+  }
+]);
